485797cdcf1085a03847ff6d8905c6a81ff29f66
Martin <martinmeneghetti@outlook.it>
2020-10-14 22:02:16 +0200

    more cats

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-// setting cat count
+// setting cats count and image count
 let count = 1;
+const maxImage = 4;
+let actualImage = 1;
+
 $("#catCount span").html(count);
 
 // updating count on click event
 $("#catImg").on("click", () => {
     count += 1;
     $("#catCount span").html(count);
-    if (count >= 10) {
-        $("#catImg").attr("src", "./img/cat2.jpg");
-    }
-    if (count >= 20) {
-        $("#catImg").attr("src", "./img/cat3.jpg");
+
+    // changing image when count is divisible by ten
+    if (count % 10 === 0) {
+        // cycling between images
+        if (actualImage < maxImage) {
+            actualImage += 1;
+        } else {
+            actualImage = 1;
+        }
+        $("#catImg").attr("src", "./img/cat" + actualImage + ".jpg");
     }
 });

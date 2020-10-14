79bb881bc98315a1f484172a71d4821006c0cfbb
Martin <martinmeneghetti@outlook.it>
2020-10-14 21:33:41 +0200

    cats

--- index.js
+++ index.js
@@ -6,4 +6,10 @@ $("#catCount span").html(count);
 $("#catImg").on("click", () => {
     count += 1;
     $("#catCount span").html(count);
+    if (count >= 10) {
+        $("#catImg").attr("src", "./img/cat2.jpg");
+    }
+    if (count >= 20) {
+        $("#catImg").attr("src", "./img/cat3.jpg");
+    }
 });

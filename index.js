7906d62755905afbc2af41f4531e7602baebb4f7
Martin <martinmeneghetti@outlook.it>
2021-03-13 22:08:44 +0100

    added Cat API

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-// setting cats count and image count
 let count = 1;
-const maxImage = 4;
-let actualImage = 1;
-
 $("#catCount span").html(count);
 
 // updating count on click event
-$("#catImg").on("click", () => {
+$("#catImg").on("click", async () => {
     count += 1;
     $("#catCount span").html(count);
 
     // changing image when count is divisible by ten
     if (count % 10 === 0) {
-        // cycling between images
-        if (actualImage < maxImage) {
-            actualImage += 1;
-        } else {
-            actualImage = 1;
-        }
-        $("#catImg").attr("src", "./img/cat" + actualImage + ".jpg");
+        // fetch new cat image
+        url = await fetchCat();
+        $("#catImg").attr("src", url);
     }
 });
+
+// fetch from Cat API a random cat image
+const fetchCat = async () => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const data = await res.json();
+
+    return data[0].url;
+};
